e77c1a22c68b69115a1052fdd8182e59417c9053
Frank-Peter Andrä <info@frankpeterandrae.de>
2026-08-24 13:17:57 +0200

    Add JumpLinkEditorPanel for managing jump links between star systems

--- StarMapGen/src/SystemEditorPanel.py
+++ StarMapGen/src/SystemEditorPanel.py
@@ -3,18 +3,13 @@ import re
 
 import wx
 
-from JumpLink import JumpLink
-from JumpLinkDialog import (
-    JumpLinkDialog,
-    JUMP_STATUS_OPTIONS,
-)
 from StarSystem import StarSystem
 from PlanetEditorPanel import (
     PlanetEditorPanel,
 )
 
-JUMP_STATUS_LABELS = dict(
-    JUMP_STATUS_OPTIONS
+from JumpLinkEditorPanel import (
+    JumpLinkEditorPanel,
 )
 
 
@@ -43,7 +38,6 @@ class SystemEditorPanel(wx.Panel):
         )
 
         self.editedPlanets = []
-        self.displayedJumpLinks = []
 
         self.createControls()
 
@@ -338,138 +332,18 @@ class SystemEditorPanel(wx.Panel):
             wx.EXPAND,
         )
 
-        editor_sizer.Add(
-            wx.StaticText(
+        self.jumpEditor = (
+            JumpLinkEditorPanel(
                 self,
-                label="Jump Links:",
-            ),
-            0,
-            wx.LEFT | wx.RIGHT | wx.TOP,
-            5,
+                self,
             )
-
-        jump_hint = wx.StaticText(
-            self,
-            label=(
-                "Add, edit, or remove jump links and assign "
-                "a route status to each connection."
-            ),
-        )
-
-        jump_hint.Wrap(330)
-
-        editor_sizer.Add(
-            jump_hint,
-            0,
-            wx.LEFT | wx.RIGHT | wx.BOTTOM | wx.EXPAND,
-            5,
-            )
-
-        self.displayedJumpLinks = []
-
-        self.jumpListControl = wx.ListCtrl(
-            self,
-            size=(-1, 120),
-            style=(
-                    wx.LC_REPORT
-                    | wx.LC_SINGLE_SEL
-                    | wx.BORDER_SUNKEN
-            ),
-        )
-
-        self.jumpListControl.InsertColumn(
-            0,
-            "System",
-            width=190,
-        )
-
-        self.jumpListControl.InsertColumn(
-            1,
-            "Status",
-            width=110,
-        )
-
-        self.jumpListControl.Bind(
-            wx.EVT_LIST_ITEM_SELECTED,
-            self.onJumpSelectionChanged,
-        )
-
-        self.jumpListControl.Bind(
-            wx.EVT_LIST_ITEM_DESELECTED,
-            self.onJumpSelectionChanged,
-        )
-
-        self.jumpListControl.Bind(
-            wx.EVT_LIST_ITEM_ACTIVATED,
-            self.editJumpLink,
-        )
-
-        self.jumpListControl.Disable()
-
-        editor_sizer.Add(
-            self.jumpListControl,
-            0,
-            wx.LEFT | wx.RIGHT | wx.BOTTOM | wx.EXPAND,
-            5,
-            )
-
-        jump_button_sizer = wx.BoxSizer(wx.HORIZONTAL)
-
-        self.addJumpButton = wx.Button(
-            self,
-            label="Add Link",
-        )
-        self.addJumpButton.Bind(
-            wx.EVT_BUTTON,
-            self.addJumpLink,
-        )
-        self.addJumpButton.Disable()
-
-        jump_button_sizer.Add(
-            self.addJumpButton,
-            0,
-            wx.RIGHT,
-            5,
-        )
-
-        self.editJumpButton = wx.Button(
-            self,
-            label="Edit Link",
-        )
-        self.editJumpButton.Bind(
-            wx.EVT_BUTTON,
-            self.editJumpLink,
-        )
-        self.editJumpButton.Disable()
-
-        jump_button_sizer.Add(
-            self.editJumpButton,
-            0,
-            wx.RIGHT,
-            5,
-        )
-
-        self.removeJumpButton = wx.Button(
-            self,
-            label="Remove Link",
-        )
-        self.removeJumpButton.Bind(
-            wx.EVT_BUTTON,
-            self.removeJumpLink,
-        )
-        self.removeJumpButton.Disable()
-
-        jump_button_sizer.Add(
-            self.removeJumpButton,
-            0,
         )
 
         editor_sizer.Add(
-            jump_button_sizer,
+            self.jumpEditor,
             0,
-            wx.LEFT | wx.RIGHT | wx.BOTTOM | wx.ALIGN_RIGHT,
-            5,
-            )
+            wx.EXPAND,
+        )
 
         editor_button_sizer = wx.BoxSizer(wx.HORIZONTAL)
 
@@ -651,7 +525,9 @@ class SystemEditorPanel(wx.Panel):
 
         self.randomizeSpectralTypesButton.Enable()
 
-        self.refreshJumpEditor(system.name)
+        self.jumpEditor.setSystem(
+            system.name
+        )
 
         self.applySystemButton.SetLabel(
             "Apply Changes"
@@ -740,7 +616,7 @@ class SystemEditorPanel(wx.Panel):
         )
 
         # A new system may be linked to any existing system.
-        self.refreshJumpEditor(None)
+        self.jumpEditor.clear()
 
         self.systemName.SetFocus()
         self.systemName.SelectAll()
@@ -1377,13 +1253,8 @@ G2, M4, WD
         self.systemSpectralTypes.SetValue("")
         self.randomizeSpectralTypesButton.Disable()
 
-        self.displayedJumpLinks = []
-        self.jumpListControl.DeleteAllItems()
-        self.jumpListControl.Disable()
+        self.jumpEditor.clear()
 
-        self.addJumpButton.Disable()
-        self.editJumpButton.Disable()
-        self.removeJumpButton.Disable()
         self.editedPlanets = []
 
         self.planetEditor.clear()
@@ -1393,263 +1264,6 @@ G2, M4, WD
         self.cancelSystemButton.Disable()
 
         self.newSystemButton.Enable(bool(self.params))
-
-    def refreshJumpEditor(self, system_name):
-        """Display the jump links belonging to one system."""
-
-        self.displayedJumpLinks = []
-        self.jumpListControl.DeleteAllItems()
-
-        if system_name is None:
-            self.jumpListControl.Disable()
-            self.updateJumpButtons()
-            return
-
-        displayed_links = []
-
-        for jump in self.jumpList:
-            if not jump.contains(system_name):
-                continue
-
-            other_name = jump.getOtherSystemName(
-                system_name
-            )
-
-            if other_name is None:
-                continue
-
-            displayed_links.append(
-                (
-                    other_name,
-                    jump,
-                )
-            )
-
-        displayed_links.sort(
-            key=lambda item: item[0].casefold()
-        )
-
-        for other_name, jump in displayed_links:
-            row = self.jumpListControl.InsertItem(
-                self.jumpListControl.GetItemCount(),
-                other_name,
-            )
-
-            self.jumpListControl.SetItem(
-                row,
-                1,
-                JUMP_STATUS_LABELS.get(
-                    jump.status,
-                    jump.status,
-                ),
-            )
-
-            self.displayedJumpLinks.append(jump)
-
-        self.jumpListControl.Enable()
-        self.updateJumpButtons()
-
-    def onJumpSelectionChanged(self, event):
-        self.updateJumpButtons()
-        event.Skip()
-
-    def updateJumpButtons(self):
-        system_name = self.getCurrentSystemName()
-
-        can_edit = (
-                system_name is not None
-                and self.getSelectedJumpLink() is not None
-                and not self.isCreatingSystem
-        )
-
-        can_add = (
-                system_name is not None
-                and bool(self.getAvailableJumpTargets())
-                and not self.isCreatingSystem
-        )
-
-        self.addJumpButton.Enable(can_add)
-        self.editJumpButton.Enable(can_edit)
-        self.removeJumpButton.Enable(can_edit)
-
-    def getCurrentSystemName(self):
-        if self.isCreatingSystem:
-            return None
-
-        index = self.selectedSystemIndex
-
-        if (
-                index == wx.NOT_FOUND
-                or not 0 <= index < len(self.starList)
-        ):
-            return None
-
-        return self.starList[index].name
-
-    def getSelectedJumpLink(self):
-        selected_row = (
-            self.jumpListControl.GetFirstSelected()
-        )
-
-        if (
-                selected_row == -1
-                or not 0 <= selected_row < len(
-            self.displayedJumpLinks
-        )
-        ):
-            return None
-
-        return self.displayedJumpLinks[selected_row]
-
-    def getAvailableJumpTargets(
-            self,
-            current_target=None,
-    ):
-        system_name = self.getCurrentSystemName()
-
-        if system_name is None:
-            return []
-
-        connected_names = {
-            jump.getOtherSystemName(system_name)
-            for jump in self.jumpList
-            if jump.contains(system_name)
-        }
-
-        return sorted(
-            [
-                system.name
-                for system in self.starList
-                if (
-                    system.name != system_name
-                    and (
-                            system.name == current_target
-                            or system.name not in connected_names
-                    )
-            )
-            ],
-            key=str.casefold,
-        )
-
-    def addJumpLink(self, event):
-        system_name = self.getCurrentSystemName()
-
-        if system_name is None:
-            return
-
-        target_names = self.getAvailableJumpTargets()
-
-        if not target_names:
-            wx.MessageBox(
-                "There are no unconnected star systems.",
-                "Add Jump Link",
-                wx.OK | wx.ICON_INFORMATION,
-                )
-            return
-
-        dialog = JumpLinkDialog(
-            self,
-            target_names,
-            title="Add Jump Link",
-        )
-
-        try:
-            if dialog.ShowModal() != wx.ID_OK:
-                return
-
-            target_name = dialog.getTargetName()
-            status = dialog.getStatus()
-        finally:
-            dialog.Destroy()
-
-        self.jumpList.append(
-            JumpLink(
-                system_name,
-                target_name,
-                status,
-            )
-        )
-
-        self.saveAndRedrawCurrentMap()
-        self.refreshJumpEditor(system_name)
-
-        self.SetStatusText(
-            f'Jump link to "{target_name}" was added.'
-        )
-
-    def editJumpLink(self, event):
-        system_name = self.getCurrentSystemName()
-        jump = self.getSelectedJumpLink()
-
-        if system_name is None or jump is None:
-            return
-
-        current_target = jump.getOtherSystemName(
-            system_name
-        )
-
-        target_names = self.getAvailableJumpTargets(
-            current_target,
-        )
-
-        dialog = JumpLinkDialog(
-            self,
-            target_names,
-            target_name=current_target,
-            status=jump.status,
-            title="Edit Jump Link",
-        )
-
-        try:
-            if dialog.ShowModal() != wx.ID_OK:
-                return
-
-            target_name = dialog.getTargetName()
-            status = dialog.getStatus()
-        finally:
-            dialog.Destroy()
-
-        jump.startName = system_name
-        jump.endName = target_name
-        jump.status = status
-
-        self.saveAndRedrawCurrentMap()
-        self.refreshJumpEditor(system_name)
-
-        self.SetStatusText(
-            f'Jump link to "{target_name}" was updated.'
-        )
-
-    def removeJumpLink(self, event):
-        system_name = self.getCurrentSystemName()
-        jump = self.getSelectedJumpLink()
-
-        if system_name is None or jump is None:
-            return
-
-        target_name = jump.getOtherSystemName(
-            system_name
-        )
-
-        result = wx.MessageBox(
-            f'Remove the jump link to "{target_name}"?',
-            "Remove Jump Link",
-            wx.YES_NO
-            | wx.NO_DEFAULT
-            | wx.ICON_QUESTION,
-            )
-
-        if result != wx.YES:
-            return
-
-        self.jumpList.remove(jump)
-
-        self.saveAndRedrawCurrentMap()
-        self.refreshJumpEditor(system_name)
-
-        self.SetStatusText(
-            f'Jump link to "{target_name}" was removed.'
-        )
 
     def deleteSelectedSystem(self, event):
         """Delete the selected star system and all of its jump links."""
@@ -1755,4 +1369,4 @@ G2, M4, WD
         )
 
         self.planetEditor.clear()
-        self.displayedJumpLinks = []
+        self.jumpEditor.clear()
